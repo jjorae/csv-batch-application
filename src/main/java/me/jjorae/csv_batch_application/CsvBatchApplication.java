9b3601955b4b30379a81c07f5319c06ea03e08ad
Raehyeok, Cho <raehyeok.cho@gmail.com>
2025-01-02 09:35:12 +0900

    프로젝트 생성

---
+++ src/main/java/me/jjorae/csv_batch_application/CsvBatchApplication.java
@@ -0,0 +1,13 @@
+package me.jjorae.csv_batch_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jjorae/csv_batch_application/CsvBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jjorae.csv_batch_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
